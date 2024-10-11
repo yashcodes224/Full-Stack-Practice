65c8d0bd61a8598a15a7738fe79f4442a0bb7ff3
Yash Singh <118042639+yashcodes224@users.noreply.github.com>
2024-10-11 17:37:23 +0530

    TopBar feature added

---
+++ blog/src/topbar/TopBar.jsx
@@ -0,0 +1,27 @@
+import "./topbar.css"
+
+export default function TopBar() {
+  return (
+    <div className="top">
+       <div className="topLeft">
+        <i className="topIcon fab fa-facebook-square"></i>
+        <i className="topIcon fab fa-twitter-square"></i>
+        <i className="topIcon fab fa-pinterest-square"></i>
+        <i className="topIcon fab fa-instagram-square"></i>
+       </div>
+       <div className="topCenter">
+        <ul className="topList">
+            <li className="topListItem">HOME</li>
+            <li className="topListItem">ABOUT</li>
+            <li className="topListItem">CONTACT</li>
+            <li className="topListItem">WRITE</li>
+            <li className="topListItem">LOGOUT</li>
+        </ul>
+       </div>
+       <div className="topRight">
+            <img className="topImg" scr="https://w0.peakpx.com/wallpaper/1002/951/HD-wallpaper-jujutsu-kaisen-op-strong-anime-gojo-satoru-love.jpg" alt=""></img>
+            <i className="topSearchIcon fas fa-search"></i>
+        </div> 
+    </div>
+  )
+}
