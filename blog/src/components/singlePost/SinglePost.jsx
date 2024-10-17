d022f696e37fbe8d2a596eea4500c868fca957ea
Yash Singh <118042639+yashcodes224@users.noreply.github.com>
2024-10-17 17:53:27 +0530

    Added post feature in the home page & created component Single Post

---
+++ blog/src/components/singlePost/SinglePost.jsx
@@ -0,0 +1,11 @@
+import "./singlepost.css"
+
+export default function SinglePost() {
+  return (
+    <div className="singlepost">
+        <div className="singlepostWrapper">
+            <img src="" alt="" className="singlepostImg" />
+        </div>
+    </div>
+  )
+}
